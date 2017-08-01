b17597eefed949b017205436c718349655def2f4
Eliaquin Encarnacion <eliaquin@gmail.com>
2017-07-31 22:35:31 -0400

    Taylor Swift

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,13 +13,14 @@ const Header = (props) => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#DD6F00',
+        backgroundColor: '#fff',
         justifyContent: 'center',
         alignItems: 'center',
         height: 60,
         shadowColor: '#000',
         shadowOffset: { width: 2, height: 2 },
-        shadowOpacity: 0.5
+        shadowOpacity: 0.5,
+        elevation: 4
         /*paddingTop: 15*/
     },
     textStyle: {
